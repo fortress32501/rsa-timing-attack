1377b47d2f1ba5f40db68be3f70a1280bb3d967b
Joshua Hill <joshuahillx1@gmail.com>
2022-11-07 12:47:06 -0600

    test 2nd

--- rsa.py
+++ rsa.py
@@ -12,7 +12,8 @@ pub = keys['pub']
 
 
 total_time = 0
-for i in range(400000):
+total_time1 = 0
+for i in range(200000):
     msg = 67055495320173175376786367218002717475 + i
     msg_bytes = msg.to_bytes(256, 'big')
     enc = rsa.encrypt_bytes(msg_bytes, pub)
@@ -23,16 +24,26 @@ for i in range(400000):
     time_diff = (end_time - start_time)
     total_time += time_diff.total_seconds()
 
+    guess = '1'
+    guess_num = int(guess, 2)
+    start_time1 = datetime.datetime.now()
+    dec1 = rsa.decrypt_bytes_guess(enc, priv, guess_num, 1)
+    end_time1 = datetime.datetime.now()
+    time_diff1 = (end_time1 - start_time1)
+    total_time1 += time_diff1.total_seconds()
+
 test=0
 print(test.from_bytes(dec,'big'))
 
 
-execution_time = total_time * 1000 / 400000
-
+execution_time = total_time * 1000 / 200000
 print(execution_time)
 
+execution_time1 = total_time1 * 1000 / 200000
+print(execution_time1)
+
 total_time = 0
-for i in range(400000): 
+for i in range(200000): 
     msg = 6705549532017317 + i
     msg_bytes = msg.to_bytes(256, 'big')
     enc = rsa.encrypt_bytes(msg_bytes, pub)
@@ -43,10 +54,8 @@ for i in range(400000):
     time_diff = (end_time - start_time)
     total_time += time_diff.total_seconds()
 
-
-
 test=0
 print(test.from_bytes(dec,'big'))
 
-execution_time = total_time * 1000 / 400000
+execution_time = total_time * 1000 / 200000
 print(execution_time)
